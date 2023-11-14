ad106f4d5298fd9f32ad3e9d050dbc527b980b35
Elvan Hidayat <elvanhd@gmail.com>
2023-11-14 23:52:06 +0700

    building the ui for the shop

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagerComponent } from './pager/pager.component';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
+
+@NgModule({
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot()],
+  exports: [PaginationModule, PagingHeaderComponent, PagerComponent],
+})
+export class SharedModule {}
