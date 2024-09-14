981fd4c831f5b00860fffb4a900a4c4e0bad12fd
Elvan Hidayat <elvanhd@gmail.com>
2024-09-14 19:57:26 +0700

    Add header and product listing with HTTP integration
    - Create HeaderComponent with navigation and cart badge
    - Integrate HTTP client for fetching product data in AppComponent
    - Display list of products dynamically in the main view
    - Add product and pagination models for type safety
    - Add new styles for header and layout using Tailwind and Angular Material
    - Include placeholder images for products and other assets
    - Update index.html to use a custom Angular Material theme
    - Modify AppComponent template to include the header and product listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { Pagination } from './shared/models/pagination';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'EcommerceShop';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => (this.products = response.data),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete'),
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
