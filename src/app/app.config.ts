39578ffdfdef3f383bd7c62dc4fc0430bf66ec4c
Elvan Hidayat <elvanhd@gmail.com>
2024-09-15 17:36:51 +0700

    Implement global error handling and loading state management
    - Add error and loading interceptors to handle HTTP responses
    - Create BusyService to manage loading state across app components
    - Implement SnackbarService for displaying success and error messages
    - Create NotFoundComponent and ServerErrorComponent for error pages
    - Add TestErrorComponent to simulate and test different error scenarios
    - Update routes to handle 404, 500, and other error status codes
    - Modify HeaderComponent to display a progress bar during HTTP requests
    - Update global styles for snackbar success and error messages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
   ],
 };
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, MatButton, MatBadge, RouterLink, RouterLinkActive],
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge,
+    RouterLink,
+    RouterLinkActive,
+    MatProgressBar,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  busyService = inject(BusyService);
+}
